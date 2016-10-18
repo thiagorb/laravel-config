5b80489d5a38c3b993e872677a17729bd2fcc323
Thiago Barcala <thiago.barcala@gmail.com>
2016-10-18 18:03:17 +0200

    Refactor tests

--- test.js
+++ test.js
@@ -1,12 +1,37 @@
 import assert from 'assert';
-import Config from './src/index';
 
-it('should be possible to retrieve configuration values', () => {
-    Config.addConfig({
-        'config key 1': 'config value 1',
-        'config key 2': 'config value 2'
-    });
+const MODULE_PATH = './src/index';
 
-    assert.equal(Config.get('config key 1'), 'config value 1');
-    assert.equal(Config.get('config key 2'), 'config value 2');
+suite('laravel-config', () => {
+	let Config;
+	
+	setup(() => {
+		Config = require(MODULE_PATH).default;
+	});
+	
+	teardown(() => {
+		delete require.cache[require.resolve(MODULE_PATH)]
+	});
+	
+	test('should be possible to retrieve configuration values', () => {
+		const Config = require('./src/index').default;
+	    Config.addConfig({
+	        'config key 1': 'config value 1',
+	        'config key 2': 'config value 2'
+	    });
+
+	    assert.equal(Config.get('config key 1'), 'config value 1');
+	    assert.equal(Config.get('config key 2'), 'config value 2');
+	});
+
+	test('should be possible to use default values', () => {
+		const Config = require('./src/index').default;
+	    Config.addConfig({
+	        'config key 1': 'config value 1'
+	    });
+
+	    assert.equal(Config.get('config key 1'), 'config value 1');
+	    assert.equal(Config.get('config key 2'), null);
+	    assert.equal(Config.get('config key 3', 'default value 3'), 'default value 3');
+	});
 });
